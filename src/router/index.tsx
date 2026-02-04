0b8b21c592d671d8a810dad3e52b9a4d351b3e44
Francisco Ibarra <56valle89@gmail.com>
2026-02-04 13:04:59 -0600

    feat: Implement Change Password page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,7 +7,7 @@ import Facturacion from '../pages/Facturacion';
 // import ReimprimirFactura from '../pages/ReimprimirFactura';
 import Producto from '../pages/Producto';
 // import Users from '../pages/Users';
-// import ChangePassword from '../pages/ChangePassword';
+import ChangePassword from '../pages/ChangePassword';
 // import AdminCaja from '../pages/AdminCaja';
 // import Admin from '../pages/Admin';
 import Login from '../pages/Login';
@@ -26,6 +26,7 @@ const AppRoutes = () => (
         <Route path="/home" element={<PrivateRoute element={<Home />} />} />
         <Route path="/producto" element={<PrivateRoute element={<Producto />} />} />
         <Route path="/facturacion" element={<PrivateRoute element={<Facturacion />} />} />
+        <Route path="/clave" element={<PrivateRoute element={<ChangePassword />} />} />
         {/* <Route path="/orders" element={<PrivateRoute element={<Orders />} />} />
         <Route path="/ordenes" element={<PrivateRoute element={<Orders />} />} />
         <Route path="/mesas" element={<PrivateRoute element={<Tables />} />} />
